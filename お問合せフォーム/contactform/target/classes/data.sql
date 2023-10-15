48b7874b697357777dcd0579867cf14a58829aa7
Sugibiz <132025719+sugibiz0908@users.noreply.github.com>
2023-10-15 19:35:19 +0900

    Delete お問合せフォーム directory

--- お問合せフォーム/contactform/target/classes/data.sql
+++
@@ -1,2 +0,0 @@
-insert IGNORE contact (id,name, email, message) values (1,'Snorlax', 'snorlax@pokemon.com', 'Snorlax is the best cute pokemon.');
-insert IGNORE contact (id,name, email, message) values (2,'Eveee', 'eveee@pokemon.com', 'Cute pokemon.');
--- お問合せフォーム/contactform/target/classes/schema.sql
+++
@@ -1,6 +0,0 @@
-create table IF NOT EXISTS contact(
-	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name varchar(255) not null,
-    email varchar(255) not null,
-    message varchar(1023) not null
-);
